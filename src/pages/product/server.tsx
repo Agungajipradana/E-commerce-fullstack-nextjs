c00bc2d21267ea0f5fcaa3cadd4f59c333763d8c
Agung Aji Pradana <agungajipradana@gmail.com>
2023-09-07 22:47:43 +0700

    add server side rendering in product/server.tsx

---
+++ src/pages/product/server.tsx
@@ -0,0 +1,26 @@
+import { ProductsProps } from "@/types";
+import ProductView from "@/views/Product";
+
+const ProductPage = (props: { products: ProductsProps[] }) => {
+  const { products } = props;
+  return (
+    <>
+      <div>
+        <ProductView products={products} />
+      </div>
+    </>
+  );
+};
+
+export default ProductPage;
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
+
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
