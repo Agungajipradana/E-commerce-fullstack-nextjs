b13a66008c1b1cdff30a36dc35a0324f4b713c6f
Agung Aji Pradana <agungajipradana@gmail.com>
2023-09-07 23:19:27 +0700

    add static site generation in product/static.tsx

---
+++ src/pages/product/static.tsx
@@ -0,0 +1,26 @@
+import { ProductsProps } from "@/types";
+import ProductView from "@/views/Product";
+
+const ProductPage = (props: { products: ProductsProps[] }) => {
+  const { products } = props;
+  return (
+    <>
+      <div>
+        <ProductView products={products} />
+      </div>
+    </>
+  );
+};
+
+export default ProductPage;
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
+
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
