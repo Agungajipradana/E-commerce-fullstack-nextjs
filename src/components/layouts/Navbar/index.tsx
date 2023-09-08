cb7992fca39ad9b2343bf950a6e42ee4e63c16b9
Agung Aji Pradana <agungajipradana@gmail.com>
2023-09-08 19:44:49 +0700

    add OAuth/Login with google

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -3,11 +3,13 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const { data }: any = useSession();
+
   return (
     <>
       <div className={styles.navbar}>
         <div className="big">Navbar</div>
-        <div>
+        <div className={styles.profile}>
+          {data?.user?.image && <img className={styles.avatar} src={data.user.image} alt={data.user.fullname} />}
           {data && data.user.fullname}
           {data ? (
             <button className={styles.button} onClick={() => signOut()}>
